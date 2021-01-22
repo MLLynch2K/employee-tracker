36c5a7b4ee6c37226dbf62f175b4eb347d26f0b8
Michele Lee Lynch <michele.lynch8@yahoo.com>
2021-01-22 12:23:45 -0500

    made changes to department id

--- db/seeds.sql
+++ db/seeds.sql
@@ -18,14 +18,13 @@ VALUES
 ('Receptionist', 35000, 1),
 ('Office Manager', 85000, 2),
 ('Sales Representative', 35000, 3), 
-('Accounting Clerk 1', 50000, 4),
-('Account Clerk 2', 30000, 5),
-('Quality Assurance Clerk', 40000, 6),
-('Purchaser', 35000, 7),
-('Customer Service Representative', 45000, 8),
-('Warehouse Assistant', 40000, 9)
-('Human Resource Representative', 55000, 10)
-('Temp', 25000, 11);
+('Accounting Clerk', 50000, 4),
+('Quality Assurance Clerk', 40000, 5),
+('Purchaser', 35000, 6),
+('Customer Service Representative', 45000, 7),
+('Warehouse Assistant', 40000, 8)
+('Human Resource Representative', 55000, 9)
+('Temp', 25000, 10);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
@@ -33,15 +32,15 @@ VALUES
 ('Dwight', 'Schrute', 3, 1),
 ('Jim', 'Halpert', 3, 1),
 ('Pam', 'Beesly', 1, 1),
-('Ryan', 'Howard', 11, 1),
+('Ryan', 'Howard', 10, 1),
 ('Andy', 'Bernard', 3, NULL),
 ('Angela', 'Martin', 4, 6),
-('Kelly', 'Kapoor', 8, NULL),
-('Toby', 'Flenderson', 10, 8),
-('Creed', 'Bratton', 6, 8),
-('Darryl', 'Philbin', 9, NULL),
-('Kevin', 'Malone', 5, 13),
-('Meredith', 'Palmer', 7, NULL),
+('Kelly', 'Kapoor', 7, NULL),
+('Toby', 'Flenderson', 9, 8),
+('Creed', 'Bratton', 5, 8),
+('Darryl', 'Philbin', 8, NULL),
+('Kevin', 'Malone', 4, 13),
+('Meredith', 'Palmer', 6, NULL),
 ('Oscar', 'Martinez', 4, 13),
 ('Phyllis', 'Vance', 3, NULL),
 ('Stanley', 'Hudson', 3, NULL);
