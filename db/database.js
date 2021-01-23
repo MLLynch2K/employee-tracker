e3d1703b4c6236184f14801734bd3a7388d99dc7
Michele Lee Lynch <michele.lynch8@yahoo.com>
2021-01-23 13:35:54 -0500

    updated mysql password

--- db/database.js
+++ db/database.js
@@ -4,9 +4,11 @@ const promisemysql = require('mysql2/promise');
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
+    // My MySQL username
     user: 'mysql -u root -p',
+    // My MySQL password
     password: 'Pupisthebest1$',
-    database: 'employee_tracker',
+    database: 'employee_tracker'
   });
 
   connection.connect(function(err) {
