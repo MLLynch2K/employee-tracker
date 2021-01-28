bf0739092fc7a25a0ef6d04bf1ae02efb41e6ff1
Michele Lee Lynch <michele.lynch8@yahoo.com>
2021-01-28 13:34:30 -0500

    changed error messages for seeds file

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,14 @@
 DROP DATABASE IF EXISTS employee_tracker;
-
 CREATE DATABASE employee_tracker;
 USE employee_tracker;
 
 CREATE TABLE departments (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(30)
 );
 
 CREATE TABLE roles (
-    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL ,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL (10,0),
     department_id INTEGER,
@@ -17,10 +16,10 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE employees (
-    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
+    manager_id INTEGER,
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id),
-    manager_id INTEGER
 );
